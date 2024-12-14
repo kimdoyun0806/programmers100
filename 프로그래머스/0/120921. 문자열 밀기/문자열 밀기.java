67188ca31b252a5bb8d6e98de6092807f241c53a
kimdoyun0806 <158257608+kimdoyun0806@users.noreply.github.com>
2024-12-15 01:58:00 +0900

    [level 0] Title: 문자열 밀기, Time: 1.55 ms, Memory: 69.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(String A, String B) {
+        int answer = 0;
+        String A_copy = A;
+        for(int i=0; i<B.length(); i++) {
+            if(A_copy.equals(B))
+                return answer;
+            // 문자열 A의 맨 뒷 문자를 가져온다
+            String a = A_copy.substring(A.length()-1);
+            // 가져온 문자 + 문자열
+            A_copy = a + A_copy.substring(0,A.length()-1);
+            answer++;
+        }
+        return -1;
+    }
+}
