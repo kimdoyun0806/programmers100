910c2171a4f4fb25e91f19bcea9f68818ee0185a
kimdoyun0806 <158257608+kimdoyun0806@users.noreply.github.com>
2024-12-14 19:04:11 +0900

    Update 옹알이 （1）.java

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -15,3 +15,23 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 사람의 문제 풀이
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        //발음할 수 있는 단어를 1로 변환 후 마지막에 1을 빈 문자열로 변환시켜 카운팅
+        for(int i =0; i < babbling.length; i++) {
+            babbling[i] = babbling[i].replace("aya", "1");
+            babbling[i] = babbling[i].replace("woo", "1");
+            babbling[i] = babbling[i].replace("ye", "1");
+            babbling[i] = babbling[i].replace("ma", "1");
+            babbling[i] = babbling[i].replace("1", "");
+            if(babbling[i].isEmpty()) {
+                answer = answer + 1;
+            }
+        }
+
+        return answer;
+    }
+}
