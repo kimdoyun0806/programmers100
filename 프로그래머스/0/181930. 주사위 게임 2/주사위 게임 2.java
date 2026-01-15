2176ab86f52b3b94ab82512715cb89d7e408c3b0
kimdoyun0806 <158257608+kimdoyun0806@users.noreply.github.com>
2026-01-15 14:17:54 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int a, int b, int c) {
+        int res = 0;
+        
+        if(a==b && b==c && c==a) {
+            return (a + b + c)
+                 * (a*a + b*b + c*c)
+                 * (a*a*a + b*b*b + c*c*c);
+        }
+        else if( a==b || b==c || c==a) {
+            return (a + b + c)
+                 * (a*a + b*b + c*c);
+        }
+        else {
+            return a + b + c;
+        }
+    }
+}
