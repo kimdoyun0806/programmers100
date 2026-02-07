9d45345198855e1743d330ad693deb9aa750cdc8
kimdoyun0806 <158257608+kimdoyun0806@users.noreply.github.com>
2026-02-07 22:37:03 +0900

    [level 0] Title: 카운트 다운, Time: 0.04 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = start_num; i >= end_num; i--) {
+            list.add(i);
+        }
+        int[] arr = new int[list.size()];
+        for(int i = 0; i < list.size(); i++ ) {
+            arr[i] = list.get(i);
+        }
+        return arr;
+    }
+}
