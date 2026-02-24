499f62d97fe4b073782b59483d3ec8351dcd1952
kimdoyun0806 <158257608+kimdoyun0806@users.noreply.github.com>
2026-02-24 17:38:39 +0900

    [level 0] Title: ad 제거하기, Time: 0.96 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] strArr) {
+        ArrayList<String> list = new ArrayList<>();
+        
+        for(int i = 0; i < strArr.length; i++) {
+            if(strArr[i].contains("ad")) {
+                continue;
+            }
+            else {
+                list.add(strArr[i]);
+            }
+        }
+        String[] answer = list.toArray(new String[0]);
+        return answer;
+    }
+}
