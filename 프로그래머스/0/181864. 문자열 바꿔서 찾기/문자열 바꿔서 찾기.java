be368bdba275810e979cbf76093be126e22f9a46
kimdoyun0806 <158257608+kimdoyun0806@users.noreply.github.com>
2026-02-26 16:48:56 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.07 ms, Memory: 85.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i < myString.length(); i++) {
+            char c = myString.charAt(i);
+            if(c == 'A') {
+                sb.append('B');
+            }
+            else {
+                sb.append('A');
+            }
+        }
+        if(sb.toString().contains(pat)) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+}
