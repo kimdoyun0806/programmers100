978e89835efcc0f10028f285f97210a5f6dddf02
kimdoyun0806 <158257608+kimdoyun0806@users.noreply.github.com>
2026-03-03 20:18:36 +0900

    [level 0] Title: 빈 배열에 추가, 삭제하기, Time: 1.69 ms, Memory: 88.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, boolean[] flag) {
+        ArrayDeque<Integer> dq = new ArrayDeque<>();
+        
+        for(int i = 0; i < arr.length; i++) {
+            int n = arr[i];
+            
+            if(flag[i] == true) {
+                int cnt = n * 2;
+                for(int j = 0; j < cnt; j++) {
+                    dq.addLast(n);
+                }
+            }
+            else {
+                for(int j = 0 ; j < n; j++) {
+                    dq.pollLast();
+                }
+            }
+        }
+        int[] result = new int[dq.size()];
+        for(int i = 0; i < result.length; i++) {
+            result[i] = dq.pollFirst();
+        }
+        return result;
+    }
+}
